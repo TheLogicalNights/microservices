918599d67634030e64a9e972c230e236630e93ad
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-06 13:03:59 +0530

    Added user microservice which will give information whether microservice is working or not

---
+++ UsersApi/src/main/java/com/microservices/UsersApi/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.microservices.UsersApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("/status")
+	public String status()
+	{
+		return "Working...";
+	}
+}
