aad01e56cb4655af9805628841e30015e0ff8754
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 15:16:50 +0530

    Added user repository

---
+++ UsersApi/src/main/java/com/microservices/UsersApi/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.microservices.UsersApi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import com.microservices.UsersApi.entity.UserEntity;
+
+@Service
+public interface UserRepo extends CrudRepository<UserEntity, Long> {
+
+}
