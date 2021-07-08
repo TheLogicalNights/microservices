1ea531ed9b14958eeb992ab52fef5f46d9c4df9c
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 15:41:46 +0530

    added password encryption

--- UsersApi/src/main/java/com/microservices/UsersApi/UsersApiApplication.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/UsersApiApplication.java
@@ -3,6 +3,8 @@ package com.microservices.UsersApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication 
 @EnableDiscoveryClient
@@ -10,6 +12,13 @@ public class UsersApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
+	}
+	
+	//for password encryption
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
 	}
 
 }
