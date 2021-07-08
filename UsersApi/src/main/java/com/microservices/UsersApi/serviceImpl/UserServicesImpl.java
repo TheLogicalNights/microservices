0d7dbb09e68782025e6f285cd82f47977df4db4d
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 14:14:08 +0530

    Added new service implementation

---
+++ UsersApi/src/main/java/com/microservices/UsersApi/serviceImpl/UserServicesImpl.java
@@ -0,0 +1,14 @@
+package com.microservices.UsersApi.serviceImpl;
+
+import com.microservices.UsersApi.services.UserServices;
+import com.microservices.UsersApi.shared.UserDto;
+
+public class UserServicesImpl implements UserServices {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
