8afe106a6d737bf8c3b7ccab19a4bde1fcfc99d8
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 15:42:14 +0530

    added password encryption

--- UsersApi/src/main/java/com/microservices/UsersApi/serviceImpl/UserServicesImpl.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/serviceImpl/UserServicesImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.microservices.UsersApi.entity.UserEntity;
@@ -17,17 +18,30 @@ public class UserServicesImpl implements UserServices {
 
 	@Autowired
 	UserRepo userRepositoryObj;
+	//for password encryption
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
+	@Autowired
+	public UserServicesImpl(BCryptPasswordEncoder bCryptPasswordEncoder) 
+	{
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+
+
 	@Override
 	public Boolean createUser(UserDto userDetails) {
+		//userid generation
 		userDetails.setUserId(UUID.randomUUID().toString());
+		//password encryption
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper = new ModelMapper();
 		//for strict mapping of member variables
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		//for mapping of member variables from source object to destination object 
 		UserEntity userEntityObj=modelMapper.map(userDetails, UserEntity.class);
-		userEntityObj.setEncryptedPassword("test");
 		userRepositoryObj.save(userEntityObj);
-		
 		return true;
 	}
 
