96ff0ef8ea65949184cd6287ef4b6ab2aedd2ca9
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 12:37:32 +0530

    Added post request handler

--- UsersApi/src/main/java/com/microservices/UsersApi/controller/UserController.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.microservices.UsersApi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.UsersApi.model.UserModel;
 
 @RestController
 @RequestMapping("/users")
@@ -12,5 +16,11 @@ public class UserController {
 	public String status()
 	{
 		return "Working...";
+	}
+	
+	@PostMapping
+	public String createUser(@RequestBody UserModel userObj)
+	{
+		return "createUser was called";
 	}
 }
