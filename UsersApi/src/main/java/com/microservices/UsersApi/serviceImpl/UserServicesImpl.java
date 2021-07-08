67dbbe39cce3ba536d777fbe300ac9ebddbd40a9
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-08 15:16:05 +0530

    Updated

--- UsersApi/src/main/java/com/microservices/UsersApi/serviceImpl/UserServicesImpl.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/serviceImpl/UserServicesImpl.java
@@ -2,15 +2,33 @@ package com.microservices.UsersApi.serviceImpl;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.microservices.UsersApi.entity.UserEntity;
+import com.microservices.UsersApi.repository.UserRepo;
 import com.microservices.UsersApi.services.UserServices;
 import com.microservices.UsersApi.shared.UserDto;
 
+@Service
 public class UserServicesImpl implements UserServices {
 
+	@Autowired
+	UserRepo userRepositoryObj;
 	@Override
-	public UserDto createUser(UserDto userDetails) {
+	public Boolean createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
-		return null;
+		ModelMapper modelMapper = new ModelMapper();
+		//for strict mapping of member variables
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		//for mapping of member variables from source object to destination object 
+		UserEntity userEntityObj=modelMapper.map(userDetails, UserEntity.class);
+		userEntityObj.setEncryptedPassword("test");
+		userRepositoryObj.save(userEntityObj);
+		
+		return true;
 	}
 
 }
