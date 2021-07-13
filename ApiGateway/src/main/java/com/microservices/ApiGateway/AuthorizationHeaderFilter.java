b5b3e08285bd545690315c7478689540472c3d28
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-13 10:59:51 +0530

    Added custom filter class for validation of jwt token

---
+++ ApiGateway/src/main/java/com/microservices/ApiGateway/AuthorizationHeaderFilter.java
@@ -0,0 +1,18 @@
+package com.microservices.ApiGateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config>{
+	public static class Config{
+		//Put configuration properties here
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
