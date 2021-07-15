13092bec70c4f9c1252959a2bf6b0f94a8623bed
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-15 13:36:50 +0530

    Created new project

---
+++ SpringConfigServer/src/main/java/com/microservices/SpringConfigServer/SpringConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.SpringConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+}
