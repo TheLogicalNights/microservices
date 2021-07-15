9ec49d3de4f72bf97d8768742e79e62f865e0d9a
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-15 14:56:04 +0530

    Updated annotation

--- SpringConfigServer/src/main/java/com/microservices/SpringConfigServer/SpringConfigServerApplication.java
+++ SpringConfigServer/src/main/java/com/microservices/SpringConfigServer/SpringConfigServerApplication.java
@@ -2,10 +2,10 @@ package com.microservices.SpringConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigServer
 public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
