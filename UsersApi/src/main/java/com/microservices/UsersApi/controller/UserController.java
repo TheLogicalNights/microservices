50a78dd449ca0026643460e3d5f91edacd49f670
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-21 18:43:27 +0530

    Added method calls

--- UsersApi/src/main/java/com/microservices/UsersApi/controller/UserController.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/controller/UserController.java
@@ -6,13 +6,16 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.UsersApi.model.UserModel;
+import com.microservices.UsersApi.model.UserResponseModel;
 import com.microservices.UsersApi.serviceImpl.UserServicesImpl;
 import com.microservices.UsersApi.shared.UserDto;
 
@@ -30,6 +33,16 @@ public class UserController {
 	public String status()
 	{
 		return "Working...with secret token - " + environment.getProperty("token.secret");
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserResponseModel> getUser(@PathVariable String userId)
+	{
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserDto userDto = userServiceImplObj.getUserByUserId(userId);
+		UserResponseModel returnValue = modelMapper.map(userDto, UserResponseModel.class);
+		return null;
 	}
 	
 	@PostMapping
