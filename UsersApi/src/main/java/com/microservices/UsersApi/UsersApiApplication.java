39d7242226805c6e16ac43538aea5b28e1944796
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-21 19:22:03 +0530

    Added bean for RestTemplate

--- UsersApi/src/main/java/com/microservices/UsersApi/UsersApiApplication.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/UsersApiApplication.java
@@ -3,8 +3,10 @@ package com.microservices.UsersApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication 
 @EnableDiscoveryClient
@@ -20,5 +22,11 @@ public class UsersApiApplication {
 	{
 		return new BCryptPasswordEncoder();
 	}
-
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
 }
