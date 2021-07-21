1a6e2ebca8d2187fa713b2cc7713e32d660dfe0b
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-21 18:44:01 +0530

    Added new posts model to store information of posts

---
+++ UsersApi/src/main/java/com/microservices/UsersApi/model/PostModel.java
@@ -0,0 +1,43 @@
+package com.microservices.UsersApi.model;
+
+import java.util.Date;
+
+public class PostModel {
+	private String postId;
+	private String userId;
+	private String postTitle;
+	private String postDescription;
+	private Date postDate;
+	public String getPostId() {
+		return postId;
+	}
+	public void setPostId(String postId) {
+		this.postId = postId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPostTitle() {
+		return postTitle;
+	}
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+	public String getPostDescription() {
+		return postDescription;
+	}
+	public void setPostDescription(String postDescription) {
+		this.postDescription = postDescription;
+	}
+	public Date getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(Date postDate) {
+		this.postDate = postDate;
+	}
+	
+	
+}
