d80b7e3c951b5ac517ba238917c845a865c512a4
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-21 18:44:34 +0530

    Added new response model

---
+++ UsersApi/src/main/java/com/microservices/UsersApi/model/UserResponseModel.java
@@ -0,0 +1,42 @@
+package com.microservices.UsersApi.model;
+
+import java.util.List;
+
+public class UserResponseModel {
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	private List<PostModel> posts;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<PostModel> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<PostModel> posts) {
+		this.posts = posts;
+	}
+}
