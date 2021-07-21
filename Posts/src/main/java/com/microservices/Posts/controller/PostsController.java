97974bcddd50e22936b6b92048abbb5dee41db70
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-21 18:01:53 +0530

    Changed signature of method get posts

--- Posts/src/main/java/com/microservices/Posts/controller/PostsController.java
+++ Posts/src/main/java/com/microservices/Posts/controller/PostsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,9 @@ public class PostsController {
 	PostsServicveImpl postServiceImplObj;
 
 	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<List<PostModel>> getPosts()
+	public ResponseEntity<List<PostModel>> getPosts(@PathVariable String id)
 	{
-		List<PostModel> returnValue = postServiceImplObj.getPosts();
+		List<PostModel> returnValue = postServiceImplObj.getPosts(id);
 		return new ResponseEntity<List<PostModel>>(returnValue, HttpStatus.OK);
 	}
 }
