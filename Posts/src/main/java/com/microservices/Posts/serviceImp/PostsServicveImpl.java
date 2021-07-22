090b969238a724e095e2c75edec6b068aded32ee
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-22 22:27:57 +0530

    Updated service implemetation method

--- Posts/src/main/java/com/microservices/Posts/serviceImp/PostsServicveImpl.java
+++ Posts/src/main/java/com/microservices/Posts/serviceImp/PostsServicveImpl.java
@@ -13,24 +13,22 @@ import com.microservices.Posts.service.PostsService;
 
 @Service
 public class PostsServicveImpl implements PostsService {
-	@Autowired
-	PostModel postModel1;
-	@Autowired
-	PostModel postModel2;
 	@Override
 	public List<PostModel> getPosts(String id) 
 	{
 		List<PostModel> returnValue = new ArrayList<>();
+		PostModel postModel1 = new PostModel();
 		postModel1.setPostId(UUID.randomUUID().toString());
 		postModel1.setUserId(id);
-		postModel1.setPostTitle("Demo post");
+		postModel1.setPostTitle("Demo post 1");
 		postModel1.setPostDescription("This is the first demo post");
 		postModel1.setPostDate(new Date());
 		
+		PostModel postModel2 = new PostModel();
 		postModel2.setPostId(UUID.randomUUID().toString());
 		postModel2.setUserId(id);
-		postModel2.setPostTitle("Demo post");
-		postModel2.setPostDescription("This is the second demo post");
+		postModel2.setPostTitle("Demo post 2");
+		postModel2.setPostDescription("This is second demo post");
 		postModel2.setPostDate(new Date());
 		
 		returnValue.add(postModel1);
