aea880dc3ab81f1a5ac1c19ce5e364fad7ee74de
Swapnil Adhav <swapniladhav560@gmail.com>
2021-07-22 21:56:22 +0530

    Added new method getPosts()

--- UsersApi/src/main/java/com/microservices/UsersApi/feign/PostFeignClient.java
+++ UsersApi/src/main/java/com/microservices/UsersApi/feign/PostFeignClient.java
@@ -1,5 +1,16 @@
 package com.microservices.UsersApi.feign;
 
-public interface PostFeignClient {
+import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.microservices.UsersApi.model.PostModel;
+
+@FeignClient(name = "posts-ws")
+public interface PostFeignClient {
+	
+	@GetMapping("/users/{id}/posts")
+	public List<PostModel> getPosts(@PathVariable String id);
 }
